77be4f0aeef172d1fd9f50cdb32149b802627643
Sean Swezey <sean.swezey@gmail.com>
2011-11-09 02:34:52 -0500

    Context menu is working well now

--- context.js
+++ context.js
@@ -1,6 +1,28 @@
-chrome.contextMenus.create({"title": "Setup a half-life password",
-							"contexts": ["all"],
-							"onclick": function(info, tab) {
-								
-							}
-						})
+var passwordMenuId;
+
+function contextFunction(info, tab) {
+	chrome.tabs.sendRequest(tab.id, {}, function(response){
+		selectedElement = response.dom_element;
+		console.log(selectedElement);
+	});
+};
+
+chrome.extension.onConnect.addListener(function(port) {
+	console.assert(port.name === "passhalf");
+	port.onMessage.addListener(handleMessages);
+});
+
+function handleMessages(msg) {
+	if(msg.password == true) {
+		passwordMenuId = chrome.contextMenus.create(
+			{"title": "Setup a half-life password",
+			 "contexts": ["all"],
+			 "onclick": contextFunction});
+		console.log(msg.passId + " " + msg.formId);
+	} else if(passwordMenuId) {
+		chrome.contextMenus.remove(passwordMenuId);
+		passwordMenuId = null;
+	} 
+}
+
+
