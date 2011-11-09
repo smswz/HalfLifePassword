16af4fa51a25c4d50414bad1e0ba9230c78caea6
Sean Swezey <sean.swezey@gmail.com>
2011-11-09 02:58:26 -0500

    Correctly getting information in context function

--- context.js
+++ context.js
@@ -1,16 +1,9 @@
-var passwordMenuId;
+// Declarations
+var passwordMenuId, formId, passId;
 
 function contextFunction(info, tab) {
-	chrome.tabs.sendRequest(tab.id, {}, function(response){
-		selectedElement = response.dom_element;
-		console.log(selectedElement);
-	});
+	console.log(formId + " " + passId);
 };
-
-chrome.extension.onConnect.addListener(function(port) {
-	console.assert(port.name === "passhalf");
-	port.onMessage.addListener(handleMessages);
-});
 
 function handleMessages(msg) {
 	if(msg.password == true) {
@@ -18,11 +11,19 @@ function handleMessages(msg) {
 			{"title": "Setup a half-life password",
 			 "contexts": ["all"],
 			 "onclick": contextFunction});
-		console.log(msg.passId + " " + msg.formId);
+		formId = msg.formId;
+		passId = msg.passId;
+		//console.log(msg.passId + " " + msg.formId);
 	} else if(passwordMenuId) {
 		chrome.contextMenus.remove(passwordMenuId);
 		passwordMenuId = null;
 	} 
 }
+
+// Scripting
+chrome.extension.onConnect.addListener(function(port) {
+	console.assert(port.name === "passhalf");
+	port.onMessage.addListener(handleMessages);
+});
 
 
