38a3d9bffee2a1773e8203cbd39c495a08c8e8a1
Timboh56 <thui@gatech.edu>
2011-12-02 16:45:55 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 // Declarations
 var contentPort, popupPort, passwordMenuId, passId;
 
+var passfields = [];
+var formfields = []
+var i = 0;
 function contextFunction(info, tab) {
 	console.log(passId);
 	mod_url = tab.url.match(/(\w*\.\w{2,3})\//)[1];
@@ -8,34 +11,47 @@ function contextFunction(info, tab) {
 }
 
 function handleContentMessages(msg) {
-	if(msg.type === "pass") {
-		if((msg.passId != null) && (!passwordMenuId)) {
-			passwordMenuId = chrome.contextMenus.create(
-				{"title": "Setup a half-life password",
-				 "contexts": ["all"],
-				 "onclick": contextFunction});
-			passId = msg.passId;
-			//console.log(msg.passId + " " + msg.formId);
-		}
-		else if((msg.passId == null) && (passwordMenuId)) {
-			chrome.contextMenus.remove(passwordMenuId);
-			passwordMenuId = null;
-		}
-	} // context menu messages
-	if(msg.type === "show"){
-		chrome.tabs.getSelected(null, function(t){
-			chrome.pageAction.show(t.id);
-		});
-		console.log(msg.fields);
-	}
+        if(msg.type === "pass") {
+                if((msg.passId != null) && (!passwordMenuId)) {
+                        passwordMenuId = chrome.contextMenus.create(
+                                {"title": "Setup a half-life password",
+                                 "contexts": ["all"],
+                                 "onclick": contextFunction});
+                        passId = msg.passId;
+                        //console.log(msg.passId + " " + msg.formId);
+                }
+                else if((msg.passId == null) && (passwordMenuId)) {
+                        chrome.contextMenus.remove(passwordMenuId);
+                        passwordMenuId = null;
+                }
+        } // context menu messages
+        if(msg.type === "show"){
+                chrome.tabs.getSelected(null, function(t){
+                chrome.pageAction.show(t.id);
+                passfields = msg.fields;
+                formfields = msg.formfields;
+                });
+        }
+}
+function handlePopupMessages(msg){
+        if(msg.type === "wantNext"){
+                if( i > passfields.length){
+                        i = 0;
+                }else{
+                        i++;
+                }
+                document.getElementById(passfields[i]).click();
+        }else if(msg.type === "wantPrev"){
+                if(i == 0){
+                        i = passfields.length - 1
+                }else{
+                        i--;
+                }
+                document.getElementById(passfields[i]).click();
+        }
+        console.log(msg.type);
 }
 
-function handlePopupMessages(msg) {
-	if(msg.type === "wantNext"){
-		//console.log(msg.type);
-	}
-	console.log(msg.type);
-}
 
 // Scripting
 chrome.extension.onConnect.addListener(function(port) {
